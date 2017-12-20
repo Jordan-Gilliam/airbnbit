97c342d2d80145d8ae8d17d99f92a01e489b74ae
Chas Campbell <chas@chascampbell.com>
2017-12-20 19:12:31 +0000

    removed orm and handlebars from server

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var express = require("express");
 var Sequelize = require("sequelize");
 var bodyParser = require("body-parser");
-var ORM = require("./config/orm.js");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -12,8 +11,6 @@ app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
