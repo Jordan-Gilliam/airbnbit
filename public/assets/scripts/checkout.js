e8ce23ac593509a2206a6685b14e390183aca988
Jordan Gilliam <gilliamja.te@gmail.com>
2017-12-24 03:56:40 +0000

    updated apiKey

--- public/assets/scripts/checkout.js
+++ public/assets/scripts/checkout.js
@@ -1,16 +1,7 @@
-exports.notification = function(req, res) {
-    console.log(req.body);
-    res.status(200);
-};
-
 var Bitpay = require('bitpay-node');
-var client = new Bitpay.Client({ apiKey: 'x50P8Udd1rIEqhQ0qSQBYaBkTRfvVauZrmJzUVH0wSg' });
+var client = new Bitpay.Client({ apiKey: 'cODHIgOxseO36CAF0BrVQNpRp6gGCWmA7mFvt5MI' });
 
-var invoiceOptions = {
-    price: 0.001,
-    currency: 'BTC'
-};
 
-client.createInvoice(invoiceOptions, function(err, invoice) {
+client.createInvoice({ price: 0.00001, currency: 'BTC' }, function(err, invoice) {
     console.log(invoice);
 });
