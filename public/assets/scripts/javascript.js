aebf4736d5a8919d26441857a667a5145ef7e344
Chas Campbell <chas@chascampbell.com>
2017-12-26 23:10:38 +0000

    minor revisions

--- public/assets/scripts/javascript.js
+++ public/assets/scripts/javascript.js
@@ -1,22 +1,9 @@
 /* global $*/
 $(document).ready(function() {
  console.log("js");
-<<<<<<< HEAD
- $.GET("/")
+ $.post("/", data)
  
 
-=======
- 
- $("#loginGuest").on("click", function(event) {
-  event.preventDefault();
-    window.location.href = 'guestData.html';
- });
- 
-  $("#loginGuest").on("click", function(event) {
-  event.preventDefault();
-    window.location.href = 'hostData.html';
- });
->>>>>>> 1859f277be562402aa18ea2ca76d1a22fddf35cf
     
 }); // End of document ready.
 
