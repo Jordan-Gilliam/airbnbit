18f7c9047436fd67d79c531bd3077b247f5d59b3
Mimi Chang <mimichang94@yahoo.com>
2018-01-02 16:08:07 +0000

    git pull

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,10 +8,10 @@ var router = express.Router();
 
 //get route to see all bookings
 
-// router.get("/api/bookings", function(req, res) {
-//     db.Booking.findAll({ include: [db.User] }).then(function(allBookings) {
-//         res.json(allBookings);
-//     });
-// });
+router.get("/api/bookings", function(req, res) {
+    db.Booking.findAll({ include: [db.User] }).then(function(allBookings) {
+        res.json(allBookings);
+    });
+});
 
 module.exports = router;
