02610a7b585f48554bb93826b73a48ceb99dfcaf
Jordan Gilliam <gilliamja.te@gmail.com>
2018-01-02 18:57:43 +0000

    checkout form redirect

--- public/assets/scripts/checkout.js
+++ public/assets/scripts/checkout.js
@@ -1,7 +1,17 @@
-var Bitpay = require('bitpay-node');
-var client = new Bitpay.Client({ apiKey: 'cODHIgOxseO36CAF0BrVQNpRp6gGCWmA7mFvt5MI' });
+// payment redirect handeler
 
-
-client.createInvoice({ price: 0.00001, currency: 'BTC' }, function(err, invoice) {
-    console.log(invoice);
-});
+function checkSelectionId() {
+    var response = document.getElementById('listId').value;
+    if (response == 1)
+        location = 'https://bitpay.com/cart/add?itemId=WYgahh4P3o4S5NV1Fu2wBe';
+    else if (response == 2)
+        location = 'https://bitpay.com/cart/add?itemId=EQ5f9Dbfgk8K3G2Xom2Kok';
+    else if (response == 3)
+        location = 'https://bitpay.com/cart/add?itemId=Um8cpRUzoAxqG8QJHdMwS9';
+    else if (response == 4)
+        location = 'https://bitpay.com/cart/add?itemId=Nd2smrtN1V7ERnf4nmyrTW';
+    else if (response == 5)
+        location = 'https://bitpay.com/cart/add?itemId=9wbCNRHkzfDZqo63BFBPa4';
+    else if (response == 6)
+        location = 'https://bitpay.com/cart/add?itemId=9Rq79fMHNecvcRe4QMF84U';
+}
