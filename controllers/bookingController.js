16dfacb8f9ed25cc7d9d9f3bae9584f9b76e09bb
Eddie Pedersen <eddiepedersen90@gmail.com>
2018-01-02 15:27:40 +0000

    controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -6,12 +6,31 @@ var express = require("express");
 
 var router = express.Router();
 
+
+
 //get route to see all bookings
 
-// router.get("/api/bookings", function(req, res) {
-//     db.Booking.findAll({ include: [db.User] }).then(function(allBookings) {
-//         res.json(allBookings);
-//     });
-// });
+router.get("/api/bookings", function(req, res) {
+    db.Booking.findAll({ include: [db.User] }).then(function(allBookings) {
+        res.json(allBookings);
+    });
+});
 
+//put route to place a new booking
+router.post("/api/bookings", function(req, res) {
+    db.Booking.findAll({ include: [db.User] }).then(function(allBookings) {
+        console.log(req.body);
+        db.Booking.create({
+            listId: req.body.listId,
+            arriveDate: req.body.arriveDate,
+            leaveDate: req.body.leaveDate
+        }).then(function(newBooking) {
+            res.json(newBooking);
+        });
+    });
+});
+
+// delete route for bookings that
+
+// PUT route for updating the bookings 
 module.exports = router;
